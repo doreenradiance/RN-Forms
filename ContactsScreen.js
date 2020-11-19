c21367d542e8e7d8c8236958f521576af154f27d
Doreen Mensah <doreenradiance@gmail.com>
2020-11-19 11:15:15 +0000

    Added ContactsScreen

--- ContactsScreen.js
+++ ContactsScreen.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, View,Text } from 'react-native';
+import Contacts from "./components/Contacts"
 
 const ContactsScreen = () => {
     contacts = [
         { name: "ama mensah", number: "2324235535" },
-        { name: "jone mensah", number: "780235535" },
+        { name: "jone man", number: "780235535" },
         { name: "gerry annan", number: "2390099009835" },
-        { name: "gabby lot", number: "2324235535" },
+        { name: "gabby lot", number: "23242359535" },
         { name: "junior addo", number: "777777777777" },
-        { name: "clara ayi", number: "54345435535" },
-        { name: "feddy nana", number: "097645566365" }
+        { name: "comfort hardy", number: "54345435535" },
+        { name: "feddy nana", number: "097645566365" },
+        { name: "clara ayi", number: "9345788535" },
+        { name: "nana king", number: "7645566365" },
     ]
     return (
         <View>
             <FlatList
                 data={contacts}
                 renderItem={({ item }) => {
-                    return <Text>{item.name}</Text>
+                    return <Contacts name={item.name} phone={item.number} />
                 }}
-                keyExtractor={(item)=>item.number}
+                keyExtractor={(item) => item.number}
             />
         </View>
     );
